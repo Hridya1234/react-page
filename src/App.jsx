b53c9fbe731e5ac7045c6c915d1a971c1afcfce2
hridya1234 <hhridyavarghese@gmail.com>
2025-02-18 14:59:59 +0530

    Updated project with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import Header from './components/dashboard/header';
-import './App.css'
+// import './App.css'
 
 function App() {
  return (
